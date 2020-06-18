ea3913fc6459be7d5cc8d41cf9146dd345fa0e9f
Douglas da silva lopes <douglas@vizir.com.br>
2020-06-17 22:47:35 -0300

    feat: create route and dto

--- Src/Points/Application/CreatePoint/CreatePointCommand.ts
+++ Src/Points/Application/CreatePoint/CreatePointCommand.ts
@@ -1,8 +1,13 @@
+import { CreatePointAddressDto } from "Points/Application/CreatePoint/CreatePointAddressDto";
+import { CreatePointProductDto } from "Points/Application/CreatePoint/CreatePointProductDto";
+
 export class CreatePointCommand {
   public constructor(
     public name: string,
+    public image: string,
     public email: string,
-    public password: string,
-    public passwordConfirmation: string,
+    public phoneMobile: string,
+    public address: CreatePointAddressDto,
+    public products: CreatePointProductDto[],
   ) {}
 }
---
+++ Src/Points/Application/CreatePoint/CreatePointProductDto.ts
@@ -0,0 +1,5 @@
+export class CreatePointProductDto {
+  public constructor(
+    public id: string,
+  ) {}
+}
